dccd3d35bba57741acba150ec3f4bf8d70f999c9
Mysterynisha <106901424+Mysterynisha@users.noreply.github.com>
2023-10-22 19:21:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-window.promises = [];
+// Add your promises to the existing array
+window.promises.push(
+    // Promise 1
+    new Promise((resolve) => {
+        // Your async logic here
+    }),
 
-// Do not change the code above this
-// add your promises to the array `promises`
+    // Promise 2
+    new Promise((resolve) => {
+        // Your async logic here
+    }),
+
+    // Promise 3
+    new Promise((resolve) => {
+        // Your async logic here
+    }),
+
+    // Promise 4
+    new Promise((resolve) => {
+        // Your async logic here
+    }),
+
+    // Promise 5
+    new Promise((resolve) => {
+        // Your async logic here
+    })
+);
+
+// Ensure you add a total of 5 promises to the array
